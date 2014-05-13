f0e1ee8ba0bcc71474d6171e7c24aab26d3d4996
Jonathan Shi <jshi@cs.cornell.edu>
2014-05-13 01:16:22 -0400

    stuff

--- code/sandbox.py
+++ code/sandbox.py
@@ -31,9 +31,12 @@ def per_file(f, X, Y):
     framesamp = 1024
     hopsamp = 80
     x = features.get_wav_data_as_feature(wav_data, framesamp, hopsamp, True)
-    y = features.get_txt_as_label_for_note(txt_filename, 60, samplerate, framesamp, hopsamp, len(x))
+    y = features.get_txt_as_label_for_note(txt_filename, 60, samplerate/8, framesamp, hopsamp, len(x))
     start, end = ut.get_trim_indices(x)
-    end = min(end, 100000)
+    trunc = 2000
+    if end-start > trunc:
+        print("\t%d windows, truncated to %d" % (end-start, trunc))
+    end = min(end, trunc+start)
 
     subx = x[start: end]
     suby = y[start: end]
@@ -55,37 +58,47 @@ def test(nfiles):
     Y = []
     X_tst = []
     Y_tst = []
-    print("Training/testing with %d files." % nfiles)
+    ntrain = nfiles
+    ntest = 3*nfiles
+    print("Training/testing with %d/%d files." % (ntrain,ntest))
     start = time.clock()   
  
     filename = '../maps/MAPS_AkPnCGdD_2/AkPnCGdD/MUS'
     files = dirs.get_files_with_extension(filename, '.mid')
     train_files = files[:nfiles]
     print("\t" + str(train_files))
-    test_files = files[nfiles:2*nfiles]
+    test_files = files[nfiles:nfiles+ntrain]
+    # for legit testing
+    # test_files = files[-3*ntest:]
     map(per_file, train_files,
       it.repeat(X, nfiles), it.repeat(Y, nfiles))
     map(per_file, test_files,
-      it.repeat(X_tst, nfiles), it.repeat(Y_tst, nfiles))
+      it.repeat(X_tst, 3*nfiles), it.repeat(Y_tst, nfiles))
 
     end = time.clock()
     print("\tRead time: %f" % (end - start))
+    print("\tnWindows train: " + str([X[i].shape[0] for i in range(len(X))]))
     start = time.clock()   
 
     crf = ChainCRF(n_states=2)
     clf = OneSlackSSVM(model=crf, C=100, n_jobs=-1,
           inference_cache=100, tol=.1)
-    clf.fit(X, Y)
+    clf.fit(np.array(X), np.array(Y))
 
     end = time.clock()
     print("\tTrain time: %f" % (end - start))
     start = time.clock()   
 
-    score = clf.score(X_tst,Y_tst)
+    Y_pred = clf.predict(X_tst)
+    comp = zip(Y_tst, Y_pred)
+    print("\tTrue positives: %d" % comp.count((1,1)))
+    print("\tTrue negatives: %d" % comp.count((0,0)))
+    print("\tFalse positives: %d" % comp.count((0,1)))
+    print("\tFalse negatives: %d" % comp.count((1,0)))
+    
 
     end = time.clock()
     print("\tTest time: %f" % (end - start))
-    print("\tCRF test set accuracy: %f" % score)
 
 
 for i in range(1,100):
